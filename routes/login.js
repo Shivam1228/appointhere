441f1d7f2520bbda3bcdb5db394f2f34f3e56374
shivam1228 <gurjarshiva9639@gamil.com>
2023-11-01 16:57:57 +0530

    login update

--- routes/login.js
+++ routes/login.js
@@ -12,24 +12,38 @@ router.get("/" ,(req,res)=>{
 
 
 //register
-router.post("/register",async(req,res)=>{
-    try{
-        const salt =await bcrypt.genSalt(10)
-        const hashedPass =await bcrypt.hash(req.body.password,salt)
-        const newUser=new User({
+router.post("/register", async (req, res) => {
+    try {
+        if (!req.body.username || !req.body.email || !req.body.password) {
+            // Handle missing data
+            return res.status(400).json({ error: "Missing data" });
+        }
+
+        // Generate a salt
+        const salt = await bcrypt.genSalt(10);
+
+        // Check if salt is a string or a number
+        if (typeof salt !== 'string' && typeof salt !== 'number') {
+            return res.status(500).json({ error: "Invalid salt" });
+        }
+
+        // Hash the password
+        const hashedPass = await bcrypt.hash(req.body.password, salt);
+
+        // Create a new user
+        const newUser = new User({
             username: req.body.username,
             email: req.body.email,
             password: hashedPass,
-        })
+        });
 
-        const user = await newUser.save()
-        res.status(200).json(user)
+        const user = await newUser.save();
+        res.status(200).json(user);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err.message });
     }
-    catch(err){
-        res.status(500).json(err)
-    }
-
-})
+});
 
 
 
@@ -37,7 +51,7 @@ router.post("/register",async(req,res)=>{
 router.post("/login", async (req,res) =>{
     try{
 
-        const user=await User.findOne({username: req.body.username})
+        const user=await User.findOne({email: req.body.email})
         !user && res.status(400).json("not a user")
         
         const validate= await bcrypt.compare(req.body.password , user.password)
